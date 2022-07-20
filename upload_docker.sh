04428248550bd1eb4576d1bd46b3ac8c1f7233f1
garimamodi123 <104634364+garimamodi123@users.noreply.github.com>
2022-07-20 17:07:53 +0530

    Create upload_docker.sh

---
+++ upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+
+dockerpath="garimamodi/project5"
+
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath" 
+docker login
+#docker tag 8ef3cf703e46 garimamodi/project4_udacity:latest
+
+# Step 3:
+# Push image to a docker repository
+#docker push <hostname>:<repository-port>/<image>:<tag>
+docker push garimamodi/project5:latest
