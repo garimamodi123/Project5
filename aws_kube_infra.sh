1981b5d8a5e926958a3f184a0c6c4a1495cf2d5c
garimamodi123 <104634364+garimamodi123@users.noreply.github.com>
2022-07-20 19:48:31 +0530

    Update aws_kube_infra.sh

--- aws_kube_infra.sh
+++ aws_kube_infra.sh
@@ -4,5 +4,5 @@ if [[ $occurance -gt 0 ]]
     echo "Since Infra is already existing hence not creating Clstr"
     else
     echo "creat AWS kube clustr"
-    eksctl create cluster --name cluster-project5-udacity --version 1.21 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 3 --managed
+    eksctl create cluster --name projectii-udacity --version 1.21 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 3 --managed
 fi
