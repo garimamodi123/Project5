8f81aa4fccc87d203d9cb27ef87b6b22db0f758b
garimamodi123 <104634364+garimamodi123@users.noreply.github.com>
2022-07-20 15:31:04 +0530

    Update prject5.py

--- prject5.py
+++ prject5.py
@@ -1,8 +1,9 @@
-import click
+#import click
 
-@click.command()
-def hello():
-    click.echo("Hello World, my name is Garima Modi")
+#@click.command()
+#def hello():
+#    click.echo("Hello World, my name is Garima Modi")
     
-if __name__=='__main__':
-    hello()
+#if __name__=='__main__':
+ #   hello()
+print("Hello World, my name is Garima Modi")
